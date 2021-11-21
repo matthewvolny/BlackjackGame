4bd28b93abc1083c982b406f7fc2e029688da503
matthewvolny <matthew.volny@gmail.com>
2021-11-20 22:06:19 -0500

    mark drawn cards used

--- scripts/main.js
+++ scripts/main.js
@@ -1,44 +1,41 @@
-/*window.addEventListener("DOMContentLoaded", function () {
-  // Execute after page load
-});
-*/
 let dealButton = document.getElementById("deal-button");
 let hitButton = document.getElementById("hit-button");
 let standButton = document.getElementById("stand-button");
 let playerHand = document.getElementById("player-hand");
 let dealerHand = document.getElementById("dealer-hand");
-// let cardImage = document.createElement("card");
-// cardImage.src = `images/${cardRank}_of_${cardSuit}.png`;
 
-// let card1 = document.querySelector(".card1");
-// let card2 = document.querySelector(".card2");
-
-//generate a random card from 'cards array'
-// let randomCard = () => {
-//   let cards = ["ace", "jack", "queen"];
-//   return cards[Math.floor(Math.random() * 3)];
-// };
-let shuffleDeck = () => {
-  // prettier-ignore
-  let ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace"];
-  let suits = ["hearts", "clubs", "diamonds", "spades"];
-
-  let deck = [];
-
-  for (let i = 0; i < ranks.length; i++) {
-    for (let j = 0; j < suits.length; j++) {
-      deck.push({ ranks: ranks[i], suits: suits[j] });
-    }
+//deck of 52 cards
+// prettier-ignore
+let ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace"];
+let suits = ["hearts", "clubs", "diamonds", "spades"];
+let deck = [];
+for (let i = 0; i < ranks.length; i++) {
+  for (let j = 0; j < suits.length; j++) {
+    deck.push({ ranks: ranks[i], suits: suits[j] });
   }
-  console.log(deck);
+}
+//draw random card
+let usedCards = [];
+let randomCard = () => {
+  let drawnCard = deck[Math.floor(Math.random() * 53)];
+  usedCards.push(drawnCard);
+  console.log(usedCards[0].ranks);
+  console.log(usedCards[0].suits);
+  // usedCards[0] = { status: "used" };
 };
-shuffleDeck();
+
+let cardImage = document.createElement("card");
+cardImage.src = "./images/10_of_spades.png";
+
+// `./images/${usedCards[0].ranks}_of_${usedCards[0].suits}.png`;
+
+randomCard();
+console.log(usedCards);
 
 //deal cards to player and Dealer when 'deal' and 'hit' button are pressed
-
 let deal = (hand, card) => {
   // hand.innerText += randomCard();
-  // hand.appendChild(card.cloneNode(true));
+  hand.appendChild(cardImage.cloneNode(true));
 };
 
 // let dealToPlayer = () => {
@@ -52,10 +49,10 @@ let deal = (hand, card) => {
 // };
 
 dealButton.addEventListener("click", function () {
-  deal(playerHand, card1);
+  deal(playerHand, randomCard());
 });
 dealButton.addEventListener("click", function () {
-  deal(dealerHand, card2);
+  deal(dealerHand, randomCard());
 });
 
 // dealButton.addEventListener("click", dealToPlayer);
