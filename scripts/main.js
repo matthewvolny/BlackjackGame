97fb1b0fcc71b7c5145451a37c6c01d9b502a71b
matthewvolny <matthew.volny@gmail.com>
2021-11-22 18:02:56 -0500

    scores are calculating, some win conditions

--- scripts/main.js
+++ scripts/main.js
@@ -27,7 +27,19 @@ function shuffleDeck(deck) {
     deck[randomCardA] = deck[randomCardB];
     deck[randomCardB] = tempX;
   }
+  return;
 }
+
+//compare player and dealer scores
+const compareScores = () => {
+  if (playerScore > dealerScore) {
+    console.log("you win!");
+  } else if (playerScore === dealerScore) {
+    console.log("it's a tie");
+  } else {
+    console.log("dealer wins!");
+  }
+};
 
 let playerCards = [];
 let dealerCards = [];
@@ -39,9 +51,53 @@ console.log(playerCards);
 
 //score players cards
 let playerScore = ""; //aces = 1 or 11
-let playerValues = []; //aces = 1
+let playerValues = [];
 
 const calculatePlayerScore = () => {
+  const reducer = (previousValue, currentValue) => previousValue + currentValue;
+  playerScore = playerValues.reduce(reducer);
+  if (playerScore === 21) {
+    console.log("you win!");
+  } else if (playerScore > 21) {
+    let index = playerValues.indexOf(11);
+    playerValues[index] = 1;
+    calculatePlayerScore();
+    //console.log("adjusted player score");
+    //console.log(playerScore);
+  }
+  console.log("playerScore");
+  console.log(playerScore);
+};
+
+//score dealers cards
+let dealerScore = ""; //aces = 1 or 11
+let dealerValues = [];
+
+const calculateDealerScore = () => {
+  // if (dealerScore > 21) {
+  //   console.log("dealer loses!");
+  // }
+  const reducer = (previousValue, currentValue) => previousValue + currentValue;
+  dealerScore = dealerValues.reduce(reducer);
+  if (dealerScore < 17) {
+    dealToDealer();
+    //document.innerHTML...;
+  } else if (dealerScore > 21) {
+    let index = dealerValues.indexOf(11);
+    dealerValues[index] = 1;
+    calculateDealerScore();
+    //console.log("adjusted player score");
+    //console.log(playerScore);
+  } else {
+    compareScores();
+  }
+  console.log("dealerScore");
+  console.log(dealerScore);
+};
+
+//convert player card ranks to values
+const calculatePlayerValues = () => {
+  playerValues = [];
   for (i = 0; i < playerCards.length; i++) {
     if (
       playerCards[i].rank === "2" ||
@@ -54,41 +110,59 @@ const calculatePlayerScore = () => {
       playerCards[i].rank === "9" ||
       playerCards[i].rank === "10"
     ) {
-      //console.log(`rank: ${playerCards[i].rank}`);
+      console.log(`rank: ${playerCards[i].rank}`);
       playerValues.push(Number(playerCards[i].rank));
     } else if (
       playerCards[i].rank === "jack" ||
       playerCards[i].rank === "queen" ||
       playerCards[i].rank === "king"
     ) {
-      //console.log(`rank: ${playerCards[i].rank}`);
+      console.log(`rank: ${playerCards[i].rank}`);
       playerValues.push(10);
     } else {
-      //console.log(`rank: ${playerCards[i].rank}`);
+      console.log(`rank: ${playerCards[i].rank}`);
       playerValues.push(11);
     }
+    calculatePlayerScore();
   }
 
-  //console.log("player values");
-  //console.log(playerValues);
+  console.log("player values");
+  console.log(playerValues);
+};
 
-  const add = () => {
-    const reducer = (previousValue, currentValue) =>
-      previousValue + currentValue;
-    return (playerScore = playerValues.reduce(reducer));
-  };
-
-  add();
-  //console.log("playerScore");
-  //console.log(playerScore);
-
-  if (playerScore > 21) {
-    let index = playerValues.indexOf(11);
-    playerValues[index] = 1;
-    add();
-    //console.log("adjusted player score");
-    //console.log(playerScore);
+//convert dealer card ranks to values
+const calculateDealerValues = () => {
+  dealerValues = [];
+  for (i = 0; i < dealerCards.length; i++) {
+    if (
+      dealerCards[i].rank === "2" ||
+      dealerCards[i].rank === "3" ||
+      dealerCards[i].rank === "4" ||
+      dealerCards[i].rank === "5" ||
+      dealerCards[i].rank === "6" ||
+      dealerCards[i].rank === "7" ||
+      dealerCards[i].rank === "8" ||
+      dealerCards[i].rank === "9" ||
+      dealerCards[i].rank === "10"
+    ) {
+      console.log(`rank: ${dealerCards[i].rank}`);
+      dealerValues.push(Number(dealerCards[i].rank));
+    } else if (
+      dealerCards[i].rank === "jack" ||
+      dealerCards[i].rank === "queen" ||
+      dealerCards[i].rank === "king"
+    ) {
+      console.log(`rank: ${dealerCards[i].rank}`);
+      playerValues.push(10);
+    } else {
+      console.log(`rank: ${dealerCards[i].rank}`);
+      dealerValues.push(11);
+    }
+    calculateDealerScore();
   }
+
+  console.log("dealer values");
+  console.log(dealerValues);
 };
 
 //put player card images out on the table
@@ -99,7 +173,6 @@ const placePlayerCardImages = (cards) => {
     cardImage.src = `./images/${cards[i].rank}_of_${cards[i].suit}.png`;
     playerHand.appendChild(cardImage);
   }
-  return;
 };
 
 //put dealer card images out on the table
@@ -109,18 +182,16 @@ const placeDealerCardImages = (cards) => {
     cardImage.src = `./images/${cards[i].rank}_of_${cards[i].suit}.png`;
     dealerHand.appendChild(cardImage);
   }
-  return;
 };
 
 //deal 2 random cards to player when deal button is pressed
 let dealToPlayer = () => {
-  playerCards.push(deck.shift());
-  calculatePlayerScore();
+  return playerCards.push(deck.shift());
 };
 
 //deal 2 random cards to dealer when deal button is pressed
 let dealToDealer = () => {
-  dealerCards.push(deck.shift());
+  return dealerCards.push(deck.shift());
   //calculateDealerScore();
 };
 
@@ -133,7 +204,19 @@ dealButton.addEventListener("click", function (e) {
   dealToDealer();
   placePlayerCardImages(playerCards);
   placeDealerCardImages(dealerCards);
+  calculatePlayerValues();
 });
 
 //hit button event handler
-hitButton.addEventListener("click", dealToPlayer);
+hitButton.addEventListener("click", function (e) {
+  dealToPlayer();
+  placePlayerCardImages(playerCards);
+  calculatePlayerValues();
+});
+
+//stand button event handler
+standButton.addEventListener("click", function (e) {
+  calculateDealerValues();
+  //dealToDealer();
+  //placeDealerCardImages(dealerCards);
+});
